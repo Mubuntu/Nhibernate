83febb1b38f003b19fe7858d1fcccbfc5be3f9d9
Mubuntu <patrick.a.frison@gmail.com>
2016-02-22 05:07:43 +0100

    start van configuratie
    aanpassing gemaakt zodat er IsessionFactory variable wordt aangemaakt
    voor de transacties

---
+++ Supportcenter/DAL/Nhibernate/Configuration.cs
@@ -0,0 +1,33 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using SC.BL.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SC.DAL.Nhibernate
+{
+    class Cfg
+    {
+        public Cfg()
+        {
+            BuildSession();
+        }
+
+        public ISessionFactory BuildSession()
+        {
+            var sessionFactory = Fluently.Configure()
+                                .Database(MsSqlConfiguration.MsSql2012
+                                .ConnectionString(c => c
+                                .FromAppSetting("SupportCenterDB_EFCodeFirst"))
+                                .ShowSql())
+                                .Mappings(m => m
+                                .FluentMappings.AddFromAssemblyOf<Ticket>().ExportTo(@"..\Nhibernate\Mapping"))
+                                .BuildSessionFactory();
+            return sessionFactory;
+        }
+    }
+}
